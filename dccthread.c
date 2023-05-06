dc7800b1900901458f868708b6d0f9b7c7ccac91
Artur <arthur.gaspar2011@gmail.com>
2023-05-06 08:32:13 -0300

    Passed up to test 9, and printing which tests passed.

--- dccthread.c
+++ dccthread.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <signal.h>
 #include <stdio.h>
 
 // Maximum number of threads and bytes on the stack of each thread.
@@ -20,6 +21,10 @@ struct dccthread {
 
 dccthread_t scheduler;
 dccthread_t* executing;
+struct sigevent sev;
+timer_t timerid;
+struct itimerspec spec;
+struct sigaction action;
 
 // Circular queue for the list of ready threads. last_ready-1 is the last value. first = last if empty.
 dccthread_t* ready_list[MAX_THREADS];
@@ -38,14 +43,16 @@ const char* dccthread_name(dccthread_t *tid) {
 void add_ready_queue(dccthread_t* thread) {
 	ready_list[last_ready] = thread;
 	last_ready = (last_ready+1)%MAX_THREADS;
-	if(last_ready == first_ready)
-		exit(1);//??????????????????????????
+	if(last_ready == first_ready) {
+		exit(1);
+	}
 	ready_siz++;
 }
 
 dccthread_t* pop_ready_queue(void) {
-	if(last_ready == first_ready)
-		exit(1);//???????????????????????
+	if(last_ready == first_ready) {
+		exit(1);
+	}
 	dccthread_t *ans = ready_list[first_ready];
 	first_ready = (first_ready+1)%MAX_THREADS;
 	ready_siz--;
@@ -61,7 +68,7 @@ dccthread_t* dccthread_create(const char *name, void (*func)(int), int param) {
 	new_thread->completed = 0;
 	new_thread->waiting_me = NULL;
 	getcontext(&new_thread->context);
-	new_thread->context.uc_link = &scheduler.context;//???????????????????????
+	new_thread->context.uc_link = &scheduler.context;
 	new_thread->context.uc_stack.ss_sp = new_thread->stack;
 	new_thread->context.uc_stack.ss_size = sizeof new_thread->stack;
 	makecontext(&new_thread->context,(void *)func,1,param);
@@ -91,29 +98,54 @@ void schedule() {
 		execute(pop_ready_queue());
 }
 
+void dccthread_sighandler(int sig) {
+	dccthread_yield();
+}
+
 void dccthread_init(void (*func)(int), int param) {
-	// Context to return to at the end
-	dccthread_t final_thread;
-	getcontext(&final_thread.context);
-	final_thread.name = "dccthread_init";
-	executing = &final_thread;
 
 	// Create scheduler thread
 	scheduler.name = "scheduler";
 	scheduler.completed = 0;
 	scheduler.waiting_me = NULL;
 	getcontext(&scheduler.context);
-	scheduler.context.uc_link = &final_thread.context;//????????????????????????????????????????
+	scheduler.context.uc_link = NULL;
 	scheduler.context.uc_stack.ss_sp = scheduler.stack;
 	scheduler.context.uc_stack.ss_size = sizeof scheduler.stack;
 	makecontext(&scheduler.context,schedule,0);
 
 	// Create main thread that will run "func"
 	dccthread_create("main",func,param);
+
+	// Creating timer
+	sev.sigev_notify = SIGEV_SIGNAL;
+	sev.sigev_signo = SIGRTMIN;
+	sev.sigev_value.sival_ptr = NULL;
+	if (timer_create(CLOCK_PROCESS_CPUTIME_ID, &sev, &timerid) == -1)
+		exit(1);
 	
+	spec.it_value.tv_sec = 0;
+	spec.it_value.tv_nsec = 10000000;
+	spec.it_interval.tv_sec = 0;
+	spec.it_interval.tv_nsec = 10000000;
+
+	// Specifying how to handle signal
+	action.sa_flags = 0;
+	sigemptyset(&action.sa_mask);
+	action.sa_handler = dccthread_sighandler;
+
+	if (sigaction(SIGRTMIN, &action, NULL) == -1)
+		exit(1);
+	
+	// Starting timer
+	if (timer_settime(timerid,0,&spec,NULL) == -1)
+		exit(1);
+
 	// Execute main thread
-	execute(pop_ready_queue());
-	exit(0);
+	while(1) {
+		executing = &scheduler;
+		setcontext(&scheduler.context);
+	}
 }
 
 
--- grade.sh
+++ grade.sh
@@ -6,43 +6,57 @@ set -eu
 
 total=22
 ecnt=0
+echo -1
 
 if ! tests/test0.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 0
 
 if ! tests/test1.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 1
 
 if ! tests/test2.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 2
 
 if ! tests/test3.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 3
 
 if ! tests/test4.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 4
 
 if ! tests/test5.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 5
 
 if ! tests/test6.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 6
 
 # This test is worth twice the points of a previous test:
 if ! tests/test7.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test7.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 7
 
 if ! tests/test8.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test8.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 8
 
 if ! tests/test9.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test9.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test9.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test9.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 9
 
 if ! tests/test10.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test10.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test10.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test10.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 10
 
 if ! tests/test11.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test11.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 11
 
 if ! tests/test12.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test12.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+echo 12
 
 
 echo "your code passes $(( $total - $ecnt )) of $total tests"
